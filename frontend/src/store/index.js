25d4420221ccdf057e121bea4ce5f59158aad37f
wildunicorn123 <amberkayeafr6901@gmail.com>
2023-08-19 14:49:01 +0200

    stuff

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,9 +6,20 @@ export default createStore({
   getters: {
   },
   mutations: {
+    ADD_PRODUCT(state, newProduct) {
+      state.products.push(newProduct);
+    }
   },
   actions: {
-  },
-  modules: {
-  }
-})
+    fetchDataFromServer() {
+      fetch("http://localhost:5002/")
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+    },
+    fetchProductById(context, productId) {
+      fetch(`http://localhost:5002/products/${productId}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+  }}})
--- frontend/src/views/ShopView.vue
+++ frontend/src/views/ShopView.vue
@@ -1,3 +1,15 @@
 <template>
 shop
 </template>
+<script>
+export default {
+  computed: {
+    projects() {
+      return this.$store.state.;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("");
+  },
+};
+</script>
